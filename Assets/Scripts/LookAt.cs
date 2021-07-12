3a9835ab31470bfbfc5668599a7e0e9b3498ce61
brad <b.bradfrancis@gmail.com>
2021-07-12 20:17:50 +1000

    Look Speed Fix

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class LookAt : MonoBehaviour
 {
+    public float lookSpeed = 10;
     public float maxXLook = 40;
     public float maxYLook = 80;
     private Vector3 pos;
@@ -16,7 +17,7 @@ public class LookAt : MonoBehaviour
     private void Update()
     {
         //pos += new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
-        pos += new Vector3(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
+        pos += new Vector3(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0) * (Time.deltaTime * lookSpeed);
         //pos.y = Mathf.Clamp(pos.y, -maxXLook, maxXLook);
         pos.x = Mathf.Clamp(pos.x, -maxYLook, maxYLook);
         transform.localRotation = Quaternion.Euler(pos);
