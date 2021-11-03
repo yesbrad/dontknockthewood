c14fe50c3a7fb9b0e3dea19b9e445da33847f2b9
Brad Francis <b.bradfrancis@gmail.com>
2021-11-03 16:59:14 +1100

    Data Push

--- Assets/Scripts/Items/ItemData.cs
+++ Assets/Scripts/Items/ItemData.cs
@@ -6,8 +6,12 @@ public class ItemData : ScriptableObject
 {
     public string name;
     public Sprite slotImageSprite;
+    public bool canGoInMouth;
 
-    [Space] 
-    
-    public SpitballData spitball;
+    [Header("Spitball")] 
+    public bool canGoInStraw;
+    [Range(1, 20)]
+    public int startAmmo;
+    [Range(0.5f, 10)] public float scoreMultiplier;
+    public Color BallColor;
 }
